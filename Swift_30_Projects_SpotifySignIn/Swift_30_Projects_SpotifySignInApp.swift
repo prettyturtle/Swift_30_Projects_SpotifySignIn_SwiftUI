accc73ed82d523b44ae16c6d01c86c3c51e5dd2a
prettyturtle <unifconti@gmail.com>
2023-05-14 17:27:01 +0900

    Initial Commit

---
+++ Swift_30_Projects_SpotifySignIn/Swift_30_Projects_SpotifySignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_30_Projects_SpotifySignInApp.swift
+//  Swift_30_Projects_SpotifySignIn
+//
+//  Created by yc on 2023/05/14.
+//
+
+import SwiftUI
+
+@main
+struct Swift_30_Projects_SpotifySignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
